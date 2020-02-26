e3099697c95ef0bf0e927dd2544621e2eaf51ece
Alexey <alexcas@mail.ru>
2020-02-26 21:24:25 +0300

    fix bug with image quality

--- app/src/main/java/com/auto/autoads/model/image/ImageResizeManager.java
+++ app/src/main/java/com/auto/autoads/model/image/ImageResizeManager.java
@@ -2,14 +2,32 @@ package com.auto.autoads.model.image;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Environment;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.Date;
 
 public class ImageResizeManager {
-    public static File saveBitmapToFile(File file){
+    public static File saveBitmapToFile(File file) {
         try {
+
+            File fileDestination = new File(
+                    Environment.getExternalStoragePublicDirectory(
+                            Environment.DIRECTORY_PICTURES
+                    ), new Date().getTime() + file.getName().substring(
+                    file.getName().lastIndexOf(".") - 1
+            )
+            );
+
+            boolean createFileResult = fileDestination.createNewFile();
+            if (!createFileResult) return null;
+
+            copy(file, fileDestination);
 
             // BitmapFactory options to downsize the image
             BitmapFactory.Options o = new BitmapFactory.Options();
@@ -17,37 +35,50 @@ public class ImageResizeManager {
             o.inSampleSize = 6;
             // factor of downsizing the image
 
-            FileInputStream inputStream = new FileInputStream(file);
+            FileInputStream inputStream = new FileInputStream(fileDestination);
             //Bitmap selectedBitmap = null;
             BitmapFactory.decodeStream(inputStream, null, o);
             inputStream.close();
 
             // The new size we want to scale to
-            final int REQUIRED_SIZE=50;
+            final int REQUIRED_SIZE = 50;
 
             // Find the correct scale value. It should be the power of 2.
             int scale = 1;
-            while(o.outWidth / scale / 2 >= REQUIRED_SIZE &&
+            while (o.outWidth / scale / 2 >= REQUIRED_SIZE &&
                     o.outHeight / scale / 2 >= REQUIRED_SIZE) {
                 scale *= 2;
             }
 
             BitmapFactory.Options o2 = new BitmapFactory.Options();
             o2.inSampleSize = scale;
-            inputStream = new FileInputStream(file);
+            inputStream = new FileInputStream(fileDestination);
 
             Bitmap selectedBitmap = BitmapFactory.decodeStream(inputStream, null, o2);
             inputStream.close();
 
             // here i override the original image file
-            file.createNewFile();
-            FileOutputStream outputStream = new FileOutputStream(file);
+            fileDestination.createNewFile();
+            FileOutputStream outputStream = new FileOutputStream(fileDestination);
 
-            selectedBitmap.compress(Bitmap.CompressFormat.JPEG, 50 , outputStream);
+            selectedBitmap.compress(Bitmap.CompressFormat.JPEG, 90, outputStream);
 
-            return file;
+            return fileDestination;
         } catch (Exception e) {
             return null;
+        }
+    }
+
+    private static void copy(File src, File dst) throws IOException {
+        try (InputStream in = new FileInputStream(src)) {
+            try (OutputStream out = new FileOutputStream(dst)) {
+                // Transfer bytes from in to out
+                byte[] buf = new byte[1024];
+                int len;
+                while ((len = in.read(buf)) > 0) {
+                    out.write(buf, 0, len);
+                }
+            }
         }
     }
 }
